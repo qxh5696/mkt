831d9899764f46defbc92ded4ddc039ed1ccb975
Qadir Haqq <haqq.q173@gmail.com>
2020-06-09 13:42:01 -0700

    Switched to mongodb and removed all of the SQLAlchemy code

--- mkt-web-app/api/api.py
+++ mkt-web-app/api/api.py
@@ -1,8 +1,90 @@
-from app import app, db
+import time
+from flask import Flask, request
+from flask_pymongo import PyMongo
+import json
+from bson import json_util, ObjectId
 
-from app.models import Question, Exam
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/MKTDataBase"
+mongo = PyMongo(app)
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Question': Question, 'Exam': Exam}
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
+
+@app.route('/exams', methods=['GET'])
+def get_exams():
+    all_exams = []
+    try:
+        for e in mongo.db.exams.find():
+            all_exams.append(e)
+        return {'exams': all_exams}
+    except Exception as e:
+        return {'response': 500, 'message': e}
+
+
+@app.route('/exam', methods=['POST'])
+def get_exam():
+    content = request.json
+    exam_id = int(content['id'])
+    exam = mongo.db.exams.find({'_id': exam_id})
+    return {'exam': exam}
+
+
+@app.route('/addExam', methods=['POST'])
+def add_exam():
+    content = request.json
+    try:
+        mongo.db.exams.insert(content)
+        return {'response': 200}
+    except Exception as e:
+        return {'response': 500, 'message': e}
+
+
+@app.route('/questions', methods=['GET'])
+def get_questions():
+    all_questions = []
+    for q in mongo.db.questions.find():
+        all_questions.append(json.loads(json_util.dumps(q)))
+    return {'questions': all_questions}
+
+
+@app.route('/addQuestion', methods=['POST'])
+def add_question():
+    content = request.json
+    try:
+        mongo.db.questions.insert(content)
+        return {'response': 200}
+    except Exception as e:
+        return {'response': 500, 'message': e}
+
+
+@app.route('/updateQuestion', methods=['POST'])
+def update_question():
+    content = request.json
+    query = {'_id': ObjectId(content['_id'])}
+    try:
+        for k, v in content.items():
+            if k != '_id':
+                mongo.db.questions.update_one(query, {'$set': {k: v}})
+        return {'response': 200}
+    except Exception as e:
+        return {'response': 500, 'message': e}
+
+
+@app.route('/deleteQuestion', methods=['POST'])
+def delete_question():
+    content = request.json
+    try:
+        result = mongo.db.questions.delete_one({'_id': ObjectId(content['id'])})
+        if result.deleteCount == 1:
+            return {'response': 200}
+        else:
+            return {'response': 404, 'message': 'Question not found matching id: ' + content['id']}
+    except Exception as e:
+        return {'response': 500, 'message': e}
+
+
 
--- mkt-web-app/api/config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# Flask db init file location: /Users/qh/Documents/python_projects/ind-study/mkt/mkt-web-app/api/migrations/alembic.ini
-# "Unable to open database file" error: https://github.com/miguelgrinberg/flasky/issues/334
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'app.db')
